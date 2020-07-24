967ebca53813cf4a323c35aaf2b548d78f3cfbe5
nu11secur1ty <venvaropt@gmail.com>
2020-07-24 23:23:30 +0300

    Update run.sh

--- Tools/thehive/run/run.sh
+++ Tools/thehive/run/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
-apt install -y docker docker-compose
+apt install -y docker docker-compose git wget 
+apt autoremove && apt update -y
 git clone https://github.com/nu11secur1ty/thehive.git
 cd thehive
 docker-compose up &
