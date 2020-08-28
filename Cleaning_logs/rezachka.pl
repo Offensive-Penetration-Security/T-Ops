6bf14b0b62474a3d01799f0fc128c1a0fbc3dfa7
nu11secur1ty <venvaropt@gmail.com>
2020-08-29 00:10:48 +0300

    Update rezachka.pl

--- Cleaning_logs/rezachka.pl
+++ Cleaning_logs/rezachka.pl
@@ -3,3 +3,61 @@ use strict;
 use warnings;
 use diagnostics;
 
+#adbhoney  
+#citrixhoneypot  
+#cowrie    
+#dionaea     
+#elk        
+#ews   
+#glutton    
+#honeypy   
+#honeytrap  
+#medpot  
+#p0f   
+#spiderfoot  
+#tanner
+#ciscoasa  
+#conpot          
+#dicompot  
+#elasticpot  
+#emobility  
+#fatt  
+#heralding  
+#honeysap  
+#mailoney   
+#nginx   
+#rdpy  
+#suricata
+
+my $al = ``;
+my $bka = ``;
+my $c = ``;
+my $d = ``;
+my $e = ``;
+my $f = ``;
+my $g = ``;
+my $i = ``;
+my $j = ``;
+my $k = ``;
+my $l = ``;
+my $m = ``;
+my $n = ``;
+my $o = ``;
+my $p = ``;
+my $q = ``;
+my $r = ``;
+my $s = ``;
+my $t = ``;
+my $u = ``;
+my $v = ``;
+my $w = ``;
+my $x = ``;
+my $y = ``;
+my $z = ``;
+my $as = ``;
+my $bas= ``;
+my $gas = ``;
+my $rfa = ``;
+my $tras = ``;
+my $man = ``;
+my $ksh = ``;
